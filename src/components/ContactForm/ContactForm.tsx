818872a12b8003c7a67363c556ebee5393e8b3a2
Eduardo Piña <mrejpm@gmail.com>
2023-01-16 19:43:19 -0400

    feat(contact): add contact page

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -6,7 +6,30 @@ const ContactForm: React.FC = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-
+  const [subject, setSubject] = useState("");
+  const [error, setError] = useState("");
+  const [sending, setSending] = useState(false);
+//   const handleSubmit = () => {
+//     if ( name === '' || email === '' || subject === '' || message === '' ){
+//         setError('all fields are required');
+//     } else{
+//         setError('');
+//         setMessageStatus('Sending...');
+//         fetch('https://portfolio-server-nodemailer.herokuapp.com/sendMail', {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//                 name,
+//                 email,
+//                 subject,
+//                 message
+//             })
+//         })
+//         .then(response => response.json())
+//         .then(data=>setMessageStatus('message sent'))
+//         .catch(error => console.error('error'));
+//     }
+// }
   const sendMessage = () => {
     console.log('====================================');
     console.log('Sending message');
@@ -18,10 +41,14 @@ const ContactForm: React.FC = () => {
     <div className={styles.Container}>
       <h1>Let's talk</h1>
       <span>Complete this form and i'll answer as soon as possible</span>
+      <span className={styles.error}>{error}</span>
     <div className={styles.ContactForm}>
       <div className="flex w-full">
       <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
         <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+      </div>
+      <div className="flex w-full">
+        <input placeholder="Subject" value={subject} onChange={e => setSubject(e.target.value)} />
       </div>
       <div className="flex w-full">
         <input placeholder="Message" value={message} onChange={e => setMessage(e.target.value)} />
