52f32237b3d90258590a9bb394caf9a66fae13e7
Eduardo Piña <mrejpm@gmail.com>
2024-11-04 12:06:23 +0100

    update text and styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,20 @@
 import React from 'react'
-import { AiFillHeart, AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiOutlineInstagram } from 'react-icons/ai/index'
-import { FaTelegramPlane } from 'react-icons/fa/index'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai/index'
 import '../../styles/text.scss'
 import styles from './Footer.module.scss'
 
 const Footer: React.FC = () => {
   return (
     <div className={styles.Footer}>
-      <div className='flex justify-center items-center'>
-        <span>Made with</span>
-        <AiFillHeart className='mx-1'/>
-        <span> by Eduardo, 2023</span>
-      </div>
+      <div></div>
       <div className={styles.Footer__socialContainer}>
         <span>Let's get in touch!</span>
         <div className='flex item-center'>
-          <a className={styles.Footer__icon} href="" target="_blank" rel="noreferrer">
+          <a className={styles.Footer__icon} href="https://github.com/edjopima" target="_blank" rel="noreferrer">
             <AiFillGithub size={20}/>
           </a>
-          <a className={styles.Footer__icon} href="" target="_blank" rel="noreferrer">
-            <AiOutlineTwitter size={20}/>
-          </a>
-          <a className={styles.Footer__icon} href="" target="_blank" rel="noreferrer">
+          <a className={styles.Footer__icon} href="https://www.linkedin.com/in/edjopima/" target="_blank" rel="noreferrer">
             <AiFillLinkedin size={20}/>
-          </a>
-          <a className={styles.Footer__icon} href="" target="_blank" rel="noreferrer">
-            <AiOutlineInstagram size={20}/>
-          </a>
-          <a className={styles.Footer__icon} href="" target="_blank" rel="noreferrer">
-            <FaTelegramPlane size={20}/>
           </a>
         </div>
       </div>
